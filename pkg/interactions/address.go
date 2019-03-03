ed3c25e3b4458c9f9fbad32f530cd6a6edc96896
Taylor Nekroze Lawson <nekroze.lawson@gmail.com>
2019-03-03 10:34:13 +1000

    Display mapped ports as well as exposed ones with the address subcommand
    Update deps and pin golang docker image version

--- pkg/interactions/address.go
+++ pkg/interactions/address.go
@@ -16,7 +16,13 @@ func ContainerAddresses(id string) error {
 	}
 
 	if info.NetworkSettings == nil {
-		return runner.UpgradeToEphemeral(errors.New("Container has no network settings"))
+		return runner.UpgradeToEphemeral(errors.New("container has no network settings"))
+	}
+
+	for _, mappings := range info.NetworkSettings.Ports {
+		for _, mapping := range mappings {
+			fmt.Printf("%s:%s\n", mapping.HostIP, mapping.HostPort)
+		}
 	}
 
 	addr := info.NetworkSettings.IPAddress
@@ -31,11 +37,11 @@ func ContainerAddresses(id string) error {
 	}
 
 	if info.Config == nil {
-		return runner.UpgradeToEphemeral(errors.New("Container has no config settings"))
+		return runner.UpgradeToEphemeral(errors.New("container has no config settings"))
 	}
 
 	if len(info.Config.ExposedPorts) == 0 {
-		return errors.New("Container has no exposed ports")
+		return errors.New("container has no exposed ports")
 	}
 	seen := map[string]bool{}
 	for port := range info.Config.ExposedPorts {
